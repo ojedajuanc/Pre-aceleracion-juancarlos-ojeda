a8116ac856b34a682cf2d73d76db89afc29fec64
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-05-28 18:19:59 -0300

    first commit

---
+++ src/main/java/com/alkemy/ojedajuanc/disney/DisneyChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.ojedajuanc.disney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyChallengeApplication.class, args);
+	}
+
+}
