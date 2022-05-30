7fb3af51882216305ee961d60b7b8baeaf4045c0
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-05-30 12:15:26 -0300

    update Character entity
    Add @Lob annotation in 'description' attribute

--- src/main/java/com/alkemy/ojedajuanc/disney/persistence/entity/Character.java
+++ src/main/java/com/alkemy/ojedajuanc/disney/persistence/entity/Character.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
@@ -36,6 +37,7 @@ public class Character {
 	@Column(name = "weight")
 	private Double weight;
 	
+	@Lob
 	@Column(name = "story")
 	private String description;
 	
